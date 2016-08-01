569dc63560ca8789f6f2126eeccd3a1e41e5d9b4
MikaelGRA <mikaelgra@gmail.com>
2016-08-01 19:47:24 +0200

    factory class added

---
+++ src/Vibrant.Tsdb.Client/TsdbFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vibrant.Tsdb.Ats;
+using Vibrant.Tsdb.Redis;
+using Vibrant.Tsdb.Sql;
+
+namespace Vibrant.Tsdb.Client
+{
+   public static class TsdbFactory
+   {
+      public static TsdbClient CreateClient( string sqlTableName, string sqlConnectionString, string atsTableNamme, string atsConnectionString )
+      {
+         var sql = new SqlPerformanceStorage( sqlTableName, sqlConnectionString );
+         var ats = new AtsVolumeStorage( atsTableNamme, atsConnectionString );
+         var sub = new DefaultPublishSubscribe( false );
+         return new TsdbClient( sql, ats, sub );
+      }
+
+      public static TsdbClient CreateClient( string sqlTableName, string sqlConnectionString, string atsTableNamme, string atsConnectionString, string redisConnectionString )
+      {
+         var sql = new SqlPerformanceStorage( sqlTableName, sqlConnectionString );
+         var ats = new AtsVolumeStorage( atsTableNamme, atsConnectionString );
+         var sub = new RedisPublishSubscribe( redisConnectionString, false );
+         return new TsdbClient( sql, ats, sub );
+      }
+
+      public static TsdbEngine CreateEngine( IWorkProvider workProvider, TsdbClient client )
+      {
+         return new TsdbEngine( workProvider, client );
+      }
+   }
+}
