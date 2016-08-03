19727887d80db155cc323165a7ce1a4f70762691
MikaelGRA <mikaelgra@gmail.com>
2016-08-03 19:31:20 +0200

    added influx db performance store

---
+++ src/Vibrant.Tsdb.InfluxDB/InfluxPerformanceStorage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vibrant.InfluxDB.Client;
+using Vibrant.InfluxDB.Client.Rows;
+
+namespace Vibrant.Tsdb.InfluxDB
+{
+   public class InfluxPerformanceStorage : IPerformanceStorage
+   {
+      private InfluxClient _client;
+
+      public InfluxPerformanceStorage( Uri endpoint, string username, string password )
+      {
+         _client = new InfluxClient( endpoint, username, password );
+      }
+
+      public Task<int> Delete( IEnumerable<string> ids )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<int> Delete( IEnumerable<string> ids, DateTime to )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<int> Delete( IEnumerable<string> ids, DateTime from, DateTime to )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<MultiReadResult<IEntry>> Read( IEnumerable<string> ids, Sort sort = Sort.Descending )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<MultiReadResult<IEntry>> Read( IEnumerable<string> ids, DateTime to, Sort sort = Sort.Descending )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<MultiReadResult<IEntry>> Read( IEnumerable<string> ids, DateTime from, DateTime to, Sort sort = Sort.Descending )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task<MultiReadResult<IEntry>> ReadLatest( IEnumerable<string> ids )
+      {
+         throw new NotImplementedException();
+      }
+
+      public Task Write( IEnumerable<IEntry> items )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
